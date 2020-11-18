ee5c3698c007f03e57f40a96c131c85ddf0ed915
BrenoGdS <breno.eletronica@gmail.com>
2020-11-17 22:30:21 -0300

    nova correção de pastas
    nova correção de pastas

--- frontend/src/exemplo/modeloGeralTemplate.php
+++ frontend/src/exemplo/modeloGeralTemplate.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="estilo1-base.css" rel="stylesheet">
+    <link href="../css/estilos.css" rel="stylesheet">
 
     <title> BSGI - Organização </title>
   <!--
