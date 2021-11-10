aadf4db6436e525d85ec6ee5972c34ba7809090a
BrenoGdS <36555827+BrenoGdS@users.noreply.github.com>
2021-11-10 20:38:43 -0300

    readequacao de padrao de nomenclatura das tabelas e campos.
    readequacao de padrao de nomenclatura das tabelas e campos.

--- scripts_sql/scripts-para-criacao-das-tabelas.sql
+++ scripts_sql/scripts-para-criacao-das-tabelas.sql
@@ -1,141 +1,141 @@
-/* BSGI_2020-2_Logico_v2: */
+/* BSGI2020-2Logicov2: */
 CREATE DATABASE BLOCOBSGI;
 
-CREATE TABLE Tb_Lider_Organizacional (
-    id_lider_organizacional INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    id_organizacao INTEGER,
-    id_usuario INTEGER
+CREATE TABLE LiderOrganizacional (
+    idliderorganizacional INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    idorganizacao INTEGER,
+    idusuario INTEGER
 );
 
-CREATE TABLE Tb_Usuario (
-    id_usuario INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nome_usuario VARCHAR(60) NOT NULL,
-    email_usuario VARCHAR(60) NOT NULL,
-    senha_usuario VARCHAR(60) NOT NULL,
-    bol_administrador SMALLINT NOT NULL,
-    cod_usuario INTEGER,
-    telefone_usuario BIGINT(11) ZEROFILL,
-    cod_tipo_sexo TINYINT(1) NOT NULL
+CREATE TABLE Usuario (
+    idusuario INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    nomeusuario VARCHAR(60) NOT NULL,
+    emailusuario VARCHAR(60) NOT NULL,
+    senhausuario VARCHAR(60) NOT NULL,
+    boladministrador SMALLINT NOT NULL,
+    codusuario INTEGER,
+    telefoneusuario BIGINT(11) ZEROFILL,
+    codtiposexo TINYINT(1) NOT NULL
 );
 
-CREATE TABLE Tb_Organizacao (
-    id_organizacao INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    id_tipo_org TINYINT(2) NOT NULL,
-    id_organizacao_pai INTEGER,
-    nome_org VARCHAR(30) NOT NULL,
-    tel_fixo_org INT(10) ZEROFILL,
-    tel_cel_org BIGINT(11) ZEROFILL,
-    email_org VARCHAR(60),
-    CEP_org INTEGER(8) ZEROFILL,
-    id_cidade_org INT(4) NOT NULL,
-    logradouro_org VARCHAR(60),
-    num_org INT(3),
-    complemento_org VARCHAR(30),
-    bairro_org VARCHAR(30)
+CREATE TABLE Organizacao (
+    idorganizacao INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    idtipoorg TINYINT(2) NOT NULL,
+    idorganizacaopai INTEGER,
+    nomeorg VARCHAR(30) NOT NULL,
+    telfixoorg INT(10) ZEROFILL,
+    telcelorg BIGINT(11) ZEROFILL,
+    emailorg VARCHAR(60),
+    CEPorg INTEGER(8) ZEROFILL,
+    idcidadeorg INT(4) NOT NULL,
+    logradouroorg VARCHAR(60),
+    numorg INT(3),
+    complementoorg VARCHAR(30),
+    bairroorg VARCHAR(30)
 );
 
-CREATE TABLE Tb_Tipo_Organizacao (
-    id_tipo_org TINYINT(2) PRIMARY KEY NOT NULL,
-    desc_tipo_org VARCHAR(30) NOT NULL
+CREATE TABLE TipoOrganizacao (
+    idtipoorg TINYINT(2) PRIMARY KEY NOT NULL,
+    desctipoorg VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Tb_Usuario_Organizacao (
-    id_usuario_organizacao INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    id_usuario INTEGER,
-    id_organizacao INTEGER
+CREATE TABLE UsuarioOrganizacao (
+    idusuarioorganizacao INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    idusuario INTEGER,
+    idorganizacao INTEGER
 );
 
-CREATE TABLE Tb_Evento (
-    id_evento INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    id_organizacao INTEGER NOT NULL,
-    id_tipo_evento TINYINT(2) NOT NULL,
+CREATE TABLE Evento (
+    idevento INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    idorganizacao INTEGER NOT NULL,
+    idtipoevento TINYINT(2) NOT NULL,
     titulo VARCHAR(60) NOT NULL,
-    data_evento DATETIME NOT NULL,
-    CEP_evento INTEGER(8) ZEROFILL,
-    id_cidade_evento INT(4),
-    logradouro_evento VARCHAR(60),
-    num_evento INT(3),
-    complemento_evento VARCHAR(30),
-    bairro_evento VARCHAR(30)
+    dataevento DATETIME NOT NULL,
+    CEPevento INTEGER(8) ZEROFILL,
+    idcidadeevento INT(4),
+    logradouroevento VARCHAR(60),
+    numevento INT(3),
+    complementoevento VARCHAR(30),
+    bairroevento VARCHAR(30)
 );
 
-CREATE TABLE Tb_Tipo_Evento (
-    id_tipo_evento TINYINT(2) PRIMARY KEY NOT NULL,
-    desc_tipo_evento VARCHAR(60) NOT NULL
+CREATE TABLE TipoEvento (
+    idtipoevento TINYINT(2) PRIMARY KEY NOT NULL,
+    desctipoevento VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE Tb_UF (
-    id_UF CHAR(2) PRIMARY KEY NOT NULL,
-    nome_UF VARCHAR(30) NOT NULL,
-    id_regiao TINYINT(1)
+CREATE TABLE UF (
+    idUF CHAR(2) PRIMARY KEY NOT NULL,
+    nomeUF VARCHAR(30) NOT NULL,
+    idregiao TINYINT(1)
 );
 
-CREATE TABLE Tb_Cidade (
-    id_cidade INT(4) PRIMARY KEY NOT NULL,
-    id_UF CHAR(2),
-    desc_cidade VARCHAR(30) NOT NULL
+CREATE TABLE Cidade (
+    idcidade INT(4) PRIMARY KEY NOT NULL,
+    idUF CHAR(2),
+    desccidade VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE Tb_Regiao (
-    id_regiao TINYINT(1) PRIMARY KEY NOT NULL,
-    desc_regiao VARCHAR(12) NOT NULL
+CREATE TABLE Regiao (
+    idregiao TINYINT(1) PRIMARY KEY NOT NULL,
+    descregiao VARCHAR(12) NOT NULL
 );
 
-CREATE TABLE tb_tipo_sexo (
-    cod_tipo_sexo TINYINT(1) PRIMARY KEY NOT NULL,
-    desc_tipo_sexo VARCHAR(12) NOT NULL
+CREATE TABLE tiposexo (
+    codtiposexo TINYINT(1) PRIMARY KEY NOT NULL,
+    desctiposexo VARCHAR(12) NOT NULL
 );
  
-ALTER TABLE Tb_Lider_Organizacional ADD CONSTRAINT FK_Tb_Lider_Organizacional_1
-    FOREIGN KEY (id_organizacao)
-    REFERENCES Tb_Organizacao (id_organizacao);
+ALTER TABLE LiderOrganizacional ADD CONSTRAINT FKLiderOrganizacional1
+    FOREIGN KEY (idorganizacao)
+    REFERENCES Organizacao (idorganizacao);
  
-ALTER TABLE Tb_Lider_Organizacional ADD CONSTRAINT FK_Tb_Lider_Organizacional_2
-    FOREIGN KEY (id_usuario)
-    REFERENCES Tb_Usuario (id_usuario);
+ALTER TABLE LiderOrganizacional ADD CONSTRAINT FKLiderOrganizacional2
+    FOREIGN KEY (idusuario)
+    REFERENCES Usuario (idusuario);
 
 
-ALTER TABLE Tb_Usuario ADD CONSTRAINT FK_Tb_Usuario_2
-    FOREIGN KEY (cod_tipo_sexo)
-    REFERENCES tb_tipo_sexo (cod_tipo_sexo);
+ALTER TABLE Usuario ADD CONSTRAINT FKUsuario2
+    FOREIGN KEY (codtiposexo)
+    REFERENCES tiposexo (codtiposexo);
 
  
-ALTER TABLE Tb_Organizacao ADD CONSTRAINT FK_Tb_Organizacao_2
-    FOREIGN KEY (id_tipo_org)
-    REFERENCES Tb_Tipo_Organizacao (id_tipo_org);
+ALTER TABLE Organizacao ADD CONSTRAINT FKOrganizacao2
+    FOREIGN KEY (idtipoorg)
+    REFERENCES TipoOrganizacao (idtipoorg);
  
-ALTER TABLE Tb_Organizacao ADD CONSTRAINT FK_Tb_Organizacao_3
-    FOREIGN KEY (id_cidade_org)
-    REFERENCES Tb_Cidade (id_cidade);
+ALTER TABLE Organizacao ADD CONSTRAINT FKOrganizacao3
+    FOREIGN KEY (idcidadeorg)
+    REFERENCES Cidade (idcidade);
  
-ALTER TABLE Tb_Organizacao ADD CONSTRAINT FK_Tb_Organizacao_4
-    FOREIGN KEY (id_organizacao_pai)
-    REFERENCES Tb_Organizacao (id_organizacao);
+ALTER TABLE Organizacao ADD CONSTRAINT FKOrganizacao4
+    FOREIGN KEY (idorganizacaopai)
+    REFERENCES Organizacao (idorganizacao);
  
-ALTER TABLE Tb_Usuario_Organizacao ADD CONSTRAINT FK_Tb_Usuario_Organizacao_1
-    FOREIGN KEY (id_organizacao)
-    REFERENCES Tb_Organizacao (id_organizacao);
+ALTER TABLE UsuarioOrganizacao ADD CONSTRAINT FKUsuarioOrganizacao1
+    FOREIGN KEY (idorganizacao)
+    REFERENCES Organizacao (idorganizacao);
  
-ALTER TABLE Tb_Usuario_Organizacao ADD CONSTRAINT FK_Tb_Usuario_Organizacao_2
-    FOREIGN KEY (id_usuario)
-    REFERENCES Tb_Usuario (id_usuario);
+ALTER TABLE UsuarioOrganizacao ADD CONSTRAINT FKUsuarioOrganizacao2
+    FOREIGN KEY (idusuario)
+    REFERENCES Usuario (idusuario);
 
-ALTER TABLE Tb_Evento ADD CONSTRAINT FK_Tb_Evento_2
-    FOREIGN KEY (id_tipo_evento)
-    REFERENCES Tb_Tipo_Evento (id_tipo_evento);
+ALTER TABLE Evento ADD CONSTRAINT FKEvento2
+    FOREIGN KEY (idtipoevento)
+    REFERENCES TipoEvento (idtipoevento);
  
-ALTER TABLE Tb_Evento ADD CONSTRAINT FK_Tb_Evento_3
-    FOREIGN KEY (id_cidade_evento)
-    REFERENCES Tb_Cidade (id_cidade);
+ALTER TABLE Evento ADD CONSTRAINT FKEvento3
+    FOREIGN KEY (idcidadeevento)
+    REFERENCES Cidade (idcidade);
  
-ALTER TABLE Tb_Evento ADD CONSTRAINT FK_Tb_Evento_4
-    FOREIGN KEY (id_organizacao)
-    REFERENCES Tb_Organizacao (id_organizacao);
+ALTER TABLE Evento ADD CONSTRAINT FKEvento4
+    FOREIGN KEY (idorganizacao)
+    REFERENCES Organizacao (idorganizacao);
  
-ALTER TABLE Tb_UF ADD CONSTRAINT FK_Tb_UF_2
-    FOREIGN KEY (id_regiao)
-    REFERENCES Tb_Regiao (id_regiao);
+ALTER TABLE UF ADD CONSTRAINT FKUF2
+    FOREIGN KEY (idregiao)
+    REFERENCES Regiao (idregiao);
  
-ALTER TABLE Tb_Cidade ADD CONSTRAINT FK_Tb_Cidade_2
-    FOREIGN KEY (id_UF)
-    REFERENCES Tb_UF (id_UF);
+ALTER TABLE Cidade ADD CONSTRAINT FKCidade2
+    FOREIGN KEY (idUF)
+    REFERENCES UF (idUF);
